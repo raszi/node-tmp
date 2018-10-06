48b9f72397c8a82b4795283c03c12a7e0ce36a53
Carsten Klein <trancesilken@gmail.com>
2018-10-06 17:49:34 +0200

    fix gh-176: fail early if there is no tmp dir specified, add rimraf dependency that somehow got lost

--- test/name-sync-test.js
+++ test/name-sync-test.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-octal */
 // vim: expandtab:ts=2:sw=2
 
-var
+const
   assert = require('assert'),
+  os = require('os'),
   inbandStandardTests = require('./name-inband-standard'),
   tmp = require('../lib/tmp');
 
@@ -35,6 +36,71 @@ describe('tmp', function () {
     });
 
     describe('when running issue specific inband tests', function () {
+      describe('on issue #176', function () {
+        const origfn = os.tmpdir;
+
+        function _generateSpecName(optsDir, osTmpDir) {
+          return 'opts.dir = "$1", os.tmpdir() = "$2"'.replace('$1', optsDir).replace('$2', osTmpDir);  
+        }
+
+        const failing = ['', '  ', undefined, null];
+        const nonFailing = ['tmp']; // the origfn cannot be trusted as the os may or may not have a valid tmp dir
+
+        describe('must fail on invalid os.tmpdir() and invalid opts.dir', function () {
+          // test all failing permutations
+          for (let oidx = 0; oidx < failing.length; oidx++) {
+            for (let iidx = 0; iidx < failing.length; iidx++) {
+              it(_generateSpecName(failing[iidx], failing[oidx]), function () {
+                os.tmpdir = function () { return failing[oidx]; };
+                try {
+                  tmp.tmpNameSync({ dir: failing[iidx] });
+                  assert.fail('expected this to fail');
+                } catch (err) {
+                  assert.ok(err instanceof Error, 'error expected');
+                } finally {
+                  os.tmpdir = origfn;
+                }
+              });
+            }
+          }
+        });
+          
+        describe('must not fail on invalid os.tmpdir() and valid opts.dir', function () {
+          // test all non failing permutations for non failing opts.dir and failing osTmpDir
+          for (let oidx = 0; oidx < failing.length; oidx++) {
+            for (let iidx = 0; iidx < nonFailing.length; iidx++) {
+              it(_generateSpecName(nonFailing[iidx], failing[oidx]), function () {
+                os.tmpdir = function () { return failing[oidx]; };
+                try {
+                  tmp.tmpNameSync({ dir: nonFailing[iidx] });
+                } catch (err) {
+                  assert.fail(err);
+                } finally {
+                  os.tmpdir = origfn;
+                }
+              });
+            }
+          }
+        });
+
+        describe('must not fail on valid os.tmpdir() and invalid opts.dir', function () {
+          // test all non failing permutations for failing opts.dir and non failing osTmpDir
+          for (let oidx = 0; oidx < nonFailing.length; oidx++) {
+            for (let iidx = 0; iidx < failing.length; iidx++) {
+              it(_generateSpecName(failing[iidx], nonFailing[oidx]), function () {
+                os.tmpdir = function () { return nonFailing[oidx]; };
+                try {
+                  tmp.tmpNameSync({ dir: failing[iidx] });
+                } catch (err) {
+                  assert.fail(err);
+                } finally {
+                  os.tmpdir = origfn;
+                }
+              });
+            }
+          }
+        });
+      });
     });
 
     describe('when running standard outband tests', function () {
