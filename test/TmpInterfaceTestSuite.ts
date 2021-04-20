ecbb2b4a8537eb24a3358cea961b5ed9f5cc88e2
silkentrance <trancesilken@gmail.com>
2021-04-20 20:28:33 +0200

    add missing tests, refactor existing

--- test/TmpInterfaceTestSuite.ts
+++ test/TmpInterfaceTestSuite.ts
@@ -1,27 +1,31 @@
 import {
-    default as tmpAsync,
+    default as asyncTmp,
     dir as asyncDir,
     file as asyncFile,
-    forceClean as asyncForceClean
+    forceClean as asyncForceClean,
+    name as asyncName
+
 } from '../src/async';
 import {
-    default as tmpPromise,
+    default as promiseTmp,
     dir as promiseDir,
     file as promiseFile,
-    forceClean as promiseForceClean
+    forceClean as promiseForceClean,
+    name as promiseName
 } from '../src/promise';
 import {
-    default as tmpSync,
+    default as syncTmp,
     dir as syncDir,
     file as syncFile,
-    forceClean as syncForceClean
+    forceClean as syncForceClean,
+    name as syncName,
 } from '../src/sync';
 
 import AsyncInterfaceImpl from '../src/internal/AsyncInterfaceImpl';
 import PromiseInterfaceImpl from '../src/internal/PromiseInterfaceImpl';
 import SyncInterfaceImpl from '../src/internal/SyncInterfaceImpl';
 
-import {suite, test} from '@testdeck/jest';
+import {params, suite, test} from '@testdeck/jest';
 import * as assert from 'assert';
 
 @suite
@@ -29,16 +33,67 @@ class TmpInterfaceTestSuite {
 
     @test
     public asyncInterfaceMustExist() {
-        assert.ok(tmpAsync instanceof AsyncInterfaceImpl);
+        assert.ok(asyncTmp instanceof AsyncInterfaceImpl);
+    }
+
+    @test
+    @params(['dir', asyncDir], 'dir')
+    @params(['file', asyncFile], 'file')
+    @params(['name', asyncName], 'name')
+    @params(['forceClean', asyncForceClean], 'forceClean')
+    public asyncFnsMustDelegateToAsyncTmp(params) {
+        this.runDelegationTest(asyncTmp, params[0], params[1]);
     }
 
     @test
     public syncInterfaceMustExist() {
-        assert.ok(tmpSync instanceof SyncInterfaceImpl);
+        assert.ok(syncTmp instanceof SyncInterfaceImpl);
+    }
+
+    @test
+    @params(['dir', syncDir], 'dir')
+    @params(['file', syncFile], 'file')
+    @params(['name', syncName], 'name')
+    @params(['forceClean', syncForceClean], 'forceClean')
+    public syncFnsMustDelegateToSyncTmp(params) {
+        this.runDelegationTest(syncTmp, params[0], params[1]);
     }
 
     @test
     public promiseInterfaceMustExist() {
-        assert.ok(tmpPromise instanceof PromiseInterfaceImpl);
+        assert.ok(promiseTmp instanceof PromiseInterfaceImpl);
+    }
+
+    @test
+    public promiseDirMustExist() {
+        assert.ok(typeof promiseDir === 'function');
+    }
+
+    @test
+    @params(['dir', promiseDir], 'dir')
+    @params(['file', promiseFile], 'file')
+    @params(['name', promiseName], 'name')
+    @params(['forceClean', promiseForceClean], 'forceClean')
+    public promiseFnsMustDelegateToPromiseTmp(params) {
+        this.runDelegationTest(promiseTmp, params[0], params[1]);
+    }
+
+    private runDelegationTest(iface, name, fn) {
+        let called = false;
+        const tester = () => {
+            called = true;
+        };
+        const oldfn = iface[name];
+        iface[name] = tester;
+        try {
+            try {
+                fn();
+            }
+            finally {
+                assert.ok(called);
+            }
+        } finally {
+            iface[name] = oldfn;
+        }
     }
 }
