7b75b1a4c1ce1ae0802e26a3d2146e7211500cae
KARASZI István <ikaraszi@gmail.com>
2022-10-31 20:20:41 +0100

    Extract availability check to its own function

--- src/name.ts
+++ src/name.ts
@@ -8,19 +8,21 @@ const isError = (e: unknown): e is Error => {
   return e !== null && typeof e === 'object' && Object.prototype.hasOwnProperty.call(e, 'code');
 };
 
+const pathAvailable = async (path: string): Promise<boolean> => {
+  try {
+    await stat(path);
+
+    return false;
+  } catch (e) {
+    return isError(e) && e.message.match(/ENOENT/) ? true : false;
+  }
+};
+
 async function create(options?: Options): Promise<string>;
 async function create(options?: Options, cb?: CallbackFunction<string>): Promise<void>;
 
 async function create(options: Options = {}, cb?: CallbackFunction<string>): Promise<void | string> {
-  const createName = createEntry(options, async (path) => {
-    try {
-      await stat(path);
-
-      return false;
-    } catch (e) {
-      return isError(e) && e.message.match(/ENOENT/) ? true : false;
-    }
-  });
+  const createName = createEntry(options, pathAvailable);
 
   return optionalCallback(createName, cb);
 }
